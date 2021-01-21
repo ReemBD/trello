0f6aea870cfb2a8b59c49baa312c706bc6fd599f
Reem Ben David <reemile1996@gmail.com>
2021-01-21 04:22:22 +0200

    added list color pick

--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -5,24 +5,34 @@ import EditIcon from '@material-ui/icons/Edit';
 import { BoardActions } from './BoardActions'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-
 export class BoardPreview extends Component {
 
     state = {
-        isBoardActionsOpen: false
+        isBoardActionsOpen: false,
+        isBoardHovered: false
     }
     toggleBoardActions = (toggle = !this.state.isBoardActionsOpen) => {
         this.setState({ isBoardActionsOpen: toggle })
     }
 
+    boardPreviewHandlers = {
+        onMouseEnter: () => {
+            this.setState({ isBoardHovered: true })
+        },
+        onMouseLeave: () => {
+            this.setState({ isBoardHovered: false })
+        }
+    }
+
     render() {
         const { board, onRemove, onEdit } = this.props
         return (
-            <div className={`board-card flex flex justify-center align-center ${this.state.isBoardActionsOpen && 'board-actions-open'}`} style={{ backgroundImage: board.style.bg }} >
+            <div {...this.boardPreviewHandlers}
+                className={`board-card flex flex justify-center align-center ${this.state.isBoardActionsOpen && 'board-actions-open'}`} style={{ backgroundImage: board.style.bg }} >
                 <Link to={`/board/${board._id}`}><h1>{board.title}</h1></Link>
                 <div className="card-overlay"></div>
                 <div className="board-actions-popover-wrapper">
-                    {<MoreHorizIcon className={`toggle-actions-icon`} onClick={this.toggleBoardActions} />}
+                    {this.state.isBoardHovered && <MoreHorizIcon className={`toggle-actions-icon`} onClick={this.toggleBoardActions} />}
                     {this.state.isBoardActionsOpen &&
                         <BoardActions
                             board={board}
@@ -31,8 +41,6 @@ export class BoardPreview extends Component {
                             {...this.props}
                             toggleBoardActions={this.toggleBoardActions} />}
                 </div>
-                <button className="icon-btn" onClick={() => onRemove(board._id)}><DeleteIcon className="icon" /> </button>
-                <button className="icon-btn edit" onClick={() => onEdit(board)}><EditIcon className="icon" /> </button>
             </div>
         )
     }
