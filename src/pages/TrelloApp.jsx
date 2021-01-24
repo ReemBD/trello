a16bc485f730a812ca34f3e485de7837bd16e500
Nofar Yunger <nofar505@gmail.com>
2021-01-24 12:24:54 +0200

    FF

--- src/pages/TrelloApp.jsx
+++ src/pages/TrelloApp.jsx
@@ -69,7 +69,7 @@ class _TrelloApp extends Component {
             <div onClick={() => {
                 setCurrPopover()
                 // if (isOverlayOpen) toggleOverlay()
-            }} style={{ paddingTop: '6vh' }}>
+            }} style={{ paddingTop: '54px' }}>
                 <div className="main-bg" style={{ backgroundImage: board.style.bg }} onClick={ev => { ev.stopPropagation() }}></div>
                 <div className={`bg-overlay `}>
                     <BoardHeader {...this.props} className={isDashboardOpen && 'dashboard-mode'} toggleDashboardNew={this.toggleDashboardNew} onToggleDashboard={this.toggleDashboard} />
